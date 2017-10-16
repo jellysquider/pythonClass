3644b78f05d5e0de6ab5a02116f610d6b1efe1d6
Gabriella Fabwen <geccentrical@gmail.com>
2017-10-15 21:54:30 -0700

    two programs matrix and chars

--- lab6/characters.py
+++ lab6/characters.py
@@ -1,3 +1,9 @@
+# Write a function that prints characters using the following header:
+# def printChars(ch1, ch2, numberPerLine): This function prints the characters
+# between ch1 and ch2 with the specified numbers per line.
+# Write a test program that prints ten characters per line from 1 to Z.
+
+
 def printChars(ch1, ch2, numberPerLine):
     i = 0;
     for entry in range(ord(ch1), ord(ch2) + 1):
--- lab6/matrix.py
+++ lab6/matrix.py
@@ -1,3 +1,8 @@
+# Write a function that displays an n-by-n matrix using the following header:
+# def printMatrix(n): Each element is 0 or 1, which is generated randomly.
+# Write a test program that prompts the user to enter n and displays an n-by-n matrix.
+
+
 from random import randint
 
 n = eval(input("Enter n: "))
